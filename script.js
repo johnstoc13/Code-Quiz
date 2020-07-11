1671a337ed214f1e753edbf2abdf84425bf6733e
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-11 15:15:51 -0400

    pseudo coding / org. thoughts

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const scoreList = document.querySelector("#score-list");
+
+
+
 function countdown() {
     const timer = setInterval(timeText, 1000);
     let timeLeft = 5;
@@ -9,3 +13,16 @@ function countdown() {
         }
     }
 }
+
+
+// NEEDS:
+// 
+// Need questions and multiple choice answers to be displayed in a form or card of sorts (maybe just as a <p>?)
+// Need to get users answer and validate true/false
+// Need to display "correct" or "incorrect"  (if incorrect, subtract time)
+// NEXT button to move on to next question
+// When time runs out, end quiz and reveal score. Also ask for user intials to be sent to HIGHSCORE page
+
+
+// <ol> for the highscores.  Add some css to every other line to make easy to read
+// Button to clear the highscores and reset the session log.
